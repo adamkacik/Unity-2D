d7edeb69a7e9d12acc42ac8d0391979ca464711c
adamkacik <45773603+adamkacik@users.noreply.github.com>
2019-02-11 21:03:42 +0100

    Add files via upload

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    /// <summary>
+    /// The Player's movement speed
+    /// </summary>
+    [SerializeField]
+    private float speed;
+
+    /// <summary>
+    /// The Player's direction
+    /// </summary>
+    protected Vector2 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        Move();
+    }
+
+    /// <summary>
+    /// Moves the player
+    /// </summary>
+    public void Move()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,46 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : Character
 {
-    [SerializeField]
-    private float speed;
-
-    private Vector2 direction;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
+    
+  
 
     // Update is called once per frame
-    void Update()
+    protected override void Update()
     {
+        //Executes the GetInput function
         GetInput();
-        Move();
+        base.Update();
+        
     }
-    public void Move()
-    {
-        transform.Translate(direction*speed*Time.deltaTime);
-    }
+    /// <summary>
+    /// Listen's to the players input
+    /// </summary>
     private void GetInput()
     {
-        direction += Vector2.zero;
+        direction = Vector2.zero;
         if (Input.GetKey(KeyCode.W))
         {
-            direction = Vector2.up;
+            direction += Vector2.up;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            direction = Vector2.down;
+            direction += Vector2.down;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            direction = Vector2.right;
+            direction += Vector2.right;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            direction = Vector2.left;
+            direction += Vector2.left;
         }
     }
 
